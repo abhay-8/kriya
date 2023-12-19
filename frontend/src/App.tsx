085ab7bd3f7516531f9307f071d99fb6eae8d853
abhay-8 <abhaynimmagadda@gmail.com>
2023-12-19 21:42:39 +0530

    updating landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import axios from "axios";
+import Modal from "react-modal"; 
+
+Modal.setAppElement("#root");
 
 const App: React.FC = () => {
   const [name, setName] = useState("");
@@ -6,6 +10,12 @@ const App: React.FC = () => {
   const [mobile, setMobile] = useState("");
   const [selectedBatch, setSelectedBatch] = useState("");
   const [gender, setGender] = useState("");
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const [userData, setUserData] = useState<any>(null); 
 
   const [errors, setErrors] = useState({
     name: "",
@@ -15,10 +25,18 @@ const App: React.FC = () => {
     gender: "",
   });
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    // Validation logic...
+    const formData = {
+      name,
+      dob,
+      mobile,
+      selectedBatch,
+      gender,
+    };
+
+    //validation on frontend
     const nameRegex = /^[a-zA-Z\s]+$/;
     const validName = nameRegex.test(name);
     const currentDate = new Date();
@@ -27,16 +45,10 @@ const App: React.FC = () => {
     const isValidDOB = age >= 18 && age <= 65;
 
     const newErrors = {
-      name:
-        !name || !validName
-          ? "Please enter a valid name"
-          : "",
-      dob:
-        !dob || !isValidDOB
-          ? "You should be between 18 and 65 years"
-          : "",
+      name: !name || !validName ? "Please enter a valid name" : "",
+      dob: !dob || !isValidDOB ? "You should be between 18 and 65 years" : "",
       mobile:
-        !mobile || mobile.length !== 10
+        !mobile || !/^\d{10}$/.test(mobile)
           ? "Please enter a valid 10-digit mobile number"
           : "",
       batch: !selectedBatch ? "Please select a batch" : "",
@@ -46,23 +58,37 @@ const App: React.FC = () => {
     const hasErrors = Object.values(newErrors).some((error) => error !== "");
 
     if (hasErrors) {
+      setErrors(newErrors);
     } else {
-      setErrors({
-        name: "",
-        dob: "",
-        mobile: "",
-        batch: "",
-        gender: "",
-      });
-      // Here, you can add your submission logic, like API calls, etc.
-      // For this example, I'll simulate a delay and then reset the form
-      setTimeout(() => {
-        setName("");
-        setDob("");
-        setMobile("");
-        setSelectedBatch("");
-        setGender("");
-      }, 1000); // Resetting form after 3 seconds (simulating a successful submission)
+      // Continue if frontend validation passes
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/api/enroll",
+          formData
+        );
+
+        if (response.status === 200) {
+          setUserData(response.data.userData); 
+          setIsOpen(true);
+          console.log("Enrolled User Data:", response.data.userData);
+          setTimeout(() => {
+            setName("");
+            setDob("");
+            setMobile("");
+            setSelectedBatch("");
+            setGender("");
+          }, 1000);
+          setErrors({
+            name: "",
+            dob: "",
+            mobile: "",
+            batch: "",
+            gender: "",
+          });
+        }
+      } catch (error) {
+        console.error("Error submitting form:", error);
+      }
     }
   };
 
@@ -82,9 +108,7 @@ const App: React.FC = () => {
         <form onSubmit={handleSubmit} className='w-full flex flex-col gap-0'>
           <div className='mb-4'>
             <label className='block  '>Name:</label>
-            {errors.name && (
-              <span className='error'>{errors.name}</span>
-            )}
+            {errors.name && <span className='error'>{errors.name}</span>}
             <input
               type='text'
               value={name}
@@ -95,9 +119,7 @@ const App: React.FC = () => {
 
           <div className='mb-4'>
             <label className='block'>Gender:</label>
-            {errors.gender && (
-              <span className='error'>{errors.gender}</span>
-            )}
+            {errors.gender && <span className='error'>{errors.gender}</span>}
             <div className='flex flex-row gap-5'>
               <label className='text-xl capitalize font-normal text-black tracking-wide'>
                 <input
@@ -134,9 +156,7 @@ const App: React.FC = () => {
           <div className='flex flex-row gap-5 w-full'>
             <div className='mb-4 w-1/3'>
               <label className='block  '>Date of Birth:</label>
-              {errors.dob && (
-              <span className='error'>{errors.dob}</span>
-            )}
+              {errors.dob && <span className='error'>{errors.dob}</span>}
               <input
                 type='date'
                 value={dob}
@@ -146,9 +166,7 @@ const App: React.FC = () => {
             </div>
             <div className='mb-4 w-2/3'>
               <label className='block'>Mobile Number:</label>
-              {errors.mobile && (
-              <span className='error'>{errors.mobile}</span>
-            )}
+              {errors.mobile && <span className='error'>{errors.mobile}</span>}
               <input
                 type='tel'
                 value={mobile}
@@ -160,9 +178,7 @@ const App: React.FC = () => {
           </div>
           <div className='mb-4'>
             <label className='block'>Select Batch:</label>
-            {errors.batch && (
-              <span className='error'>{errors.batch}</span>
-            )}
+            {errors.batch && <span className='error'>{errors.batch}</span>}
             <select
               value={selectedBatch}
               onChange={(e) => setSelectedBatch(e.target.value)}
@@ -191,6 +207,26 @@ const App: React.FC = () => {
           <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-black opacity-80'></div>
         </div>
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel='Success Modal'
+        className='bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 rounded-lg shadow-lg'>
+        <h2 className='text-green-800 text-3xl font-bold mb-5'>
+          You have successfully enrolled!
+        </h2>
+        <p className='text-lg'>Name: {userData?.name}</p>
+        <p className='text-lg'>Gender: {userData?.gender}</p>
+        <p className='text-lg'>Date of Birth: {userData?.dob}</p>
+        <p className='text-lg'>Mobile Number: {userData?.mobile}</p>
+        <p className='text-lg'>Selected Batch: {userData?.selectedBatch}</p>
+
+        <button
+          className='bg-black text-white border-black border-2 p-2 mt-5'
+          onClick={closeModal}>
+          Close
+        </button>
+      </Modal>
     </div>
   );
 };
